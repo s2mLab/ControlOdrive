bb96345008f066ea7c1e2a9d675005233ecba43a
= <=>
2023-06-15 19:18:28 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 """
 This file is used to build the project.
+sudo python setup.py install
+sudo pip uninstall ergocycleS2M
 """
 from setuptools import setup
 
@@ -25,7 +27,7 @@ setup(
     entry_points={
         "console_scripts": [
             "read_ergocycle_file = ergocycleS2M.data_processing.load:read_from_terminal",
-            "start_ergocycle = ergocycleS2M.gui.main:main",
+            "start_ergocycle = main:main",
         ],
     },
 )
